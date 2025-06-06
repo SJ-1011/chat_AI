fd01383f7ff5e4b8257d8a3a7d5f07daf9bbf6e2
Seonjin <ebngk0@gmail.com>
2025-06-06 15:21:54 +0900

    ✨ Chatting 채팅창 기능 구현 - `<textarea>`로 사용자에게 메시지를 입력 받음 - UI 구현

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-function Chat() {
+interface ChatProps {
+  setIsHidden: (hidden: boolean) => void;
+  setQuestionMessage: (message: string) => void;
+}
+
+function Chat({ setIsHidden, setQuestionMessage }: ChatProps) {
   const [message, setMessage] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(message);
     setMessage("");
+
+    setIsHidden(true);
+    setQuestionMessage(message);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="메시지를 입력하세요..." rows={3} className="w-full p-2 border rounded resize-none" />
-      <button type="submit" className="mt-2 bg-[#3399FF] text-white p-2 rounded">
+    <form onSubmit={handleSubmit} className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col w-fit">
+      <textarea
+        ref={textareaRef}
+        value={message}
+        onChange={handleChange}
+        placeholder="메시지를 입력하세요..."
+        rows={3}
+        className="min-w-[6.25rem] lg:min-w-[50rem] p-4 border-2 border-[#cce5ff] rounded-4xl resize-none overflow-hidden"
+      />
+      <button type="submit" className="self-end mt-4 bg-[#3399FF] text-white p-2 rounded-4xl w-20 cursor-pointer hover:bg-[#2A82E6] transition-all">
         전송
       </button>
     </form>
--- src/pages/Chatting.tsx
+++ src/pages/Chatting.tsx
@@ -1,12 +1,17 @@
-// import { useState } from "react";
+import { useState } from "react";
 import Chat from "../components/Chat";
+import Messenger from "../components/Messenger";
 
 function Chatting() {
+  const [isHidden, setIsHidden] = useState(false);
+  const [questionMessage, setQuestionMessage] = useState("");
+
   return (
-    <main className="w-full">
-      <h1 className="text-center mt-[12.5rem] text-5xl ">Chat AI</h1>
-      <h2 className="text-center mt-8 text-3xl">나만의 웹 페이지에서 AI에게 질문하세요!</h2>
-      <Chat />
+    <main className="w-full h-screen relative">
+      <h1 className={`text-center mt-[12.5rem] text-5xl ${isHidden ? "hidden" : ""}`}>Chat AI</h1>
+      <h2 className={`text-center mt-8 text-3xl ${isHidden ? "hidden" : ""}`}>나만의 웹 페이지에서 AI에게 질문하세요!</h2>
+      <Messenger questionMessage={questionMessage} />
+      <Chat setIsHidden={setIsHidden} setQuestionMessage={setQuestionMessage} />
     </main>
   );
 }
